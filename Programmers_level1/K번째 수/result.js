db38261d761aa3b83fb8f18d37575dfd84c1f8d4
93jm <65124480+93jm@users.noreply.github.com>
2021-04-22 03:03:07 +0900

    2021-04-22

---
+++ Programmers_level1/K번째 수/result.js
@@ -0,0 +1,11 @@
+function solution(array, commands) {
+  var answer = [];
+  for (i = 0; i < commands.length; i++) {
+    const checker = commands[i][2] - 1;
+    const subArr = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+    answer.push(subArr[checker]);
+  }
+  return answer;
+}
